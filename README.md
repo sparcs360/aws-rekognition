98bfe4386f046cbe74901007d0432344c9903172
Lee Newfeld <lee.newfeld@gmail.com>
2018-04-22 16:58:37 +0100

    Index and recognise faces

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # aws-rekognition
 Playing with AWS Rekognition (in Python3)
+
+# Searching Faces in a Collection
+
+https://docs.aws.amazon.com/rekognition/latest/dg/collections.html
+
+## Create a collection of known faces
+
+```bash
+$ aws rekognition create-collection --collection-id lnewfeld
+```
+
+```bash
+$ aws rekognition list-collections
+{
+    "FaceModelVersions": [
+        "3.0"
+    ],
+    "CollectionIds": [
+        "lnewfeld"
+    ]
+}
+```
+
+\(index a face by pressing `i` in `capture_testrig.py`)
+
+```bash
+$ aws rekognition list-faces --collection-id lnewfeld
+{
+    "FaceModelVersion": "3.0",
+    "Faces": [
+        {
+            "BoundingBox": {
+                "Width": 0.29326900839805603,
+                "Top": 0.31410300731658936,
+                "Left": 0.4699519872665405,
+                "Height": 0.39102599024772644
+            },
+            "FaceId": "a132d053-6549-4b86-9332-fea5167e868c",
+            "ExternalImageId": "test",
+            "Confidence": 99.99970245361328,
+            "ImageId": "907c0e14-5268-52f2-9d78-1f38a2ad4cce"
+        }
+    ]
+}
+```
+
+```bash
+$ aws rekognition delete-faces --collection-id lnewfeld --face-ids a132d053-6549-4b86-9332-fea5167e868c
+{
+    "DeletedFaces": [
+        "a132d053-6549-4b86-9332-fea5167e868c"
+    ]
+}
+```
--- capture_testrig.py
+++ capture_testrig.py
@@ -2,22 +2,29 @@
 import cv2
 import numpy as np
 import boto3
+from botocore.exceptions import ClientError
+
+def parse_command_line():
+    import re
+
+    def valid_face_name(value, regex=re.compile(r"^[a-zA-Z0-9_.\-:]+$")):
+        if not regex.match(value):
+            raise argparse.ArgumentTypeError("value must match regex {}".format(regex.pattern))
+        return value
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--region", help="AWS region to use", default="eu-west-1")
+    parser.add_argument("--collection-id", help="AWS rekognition collection to store known faces in",
+                        default="lnewfeld")
+    parser.add_argument("--face-name", type=valid_face_name, help="Who's in front of the camera?", default="NOONE")
+    args = parser.parse_args()
+    print("{}".format(args))
+    return args
 
 
-def create_rekognition_client(region='eu-west-1'):
+def create_rekognition_client(region):
     return boto3.client('rekognition', region)
-
-
-def create_rekognition_request(bucket='lnewfeld', filename=None, image_data=None):
-    if not ((filename is None) ^ (image_data is None)):
-        raise ValueError("filename xor image_data must be provided")
-
-    if image_data is None:
-        request = {'S3Object': {'Bucket': bucket, 'Name': filename}}
-    else:
-        request = {'Bytes': image_data}
-
-    return request
 
 
 def video_frame_to_jpeg_string(frame):
@@ -28,54 +35,92 @@ def video_frame_to_jpeg_string(frame):
 
 def detect_objects(frame):
     print('Detecting objects...')
-    request = create_rekognition_request(image_data=video_frame_to_jpeg_string(frame))
+    request = {'Bytes': video_frame_to_jpeg_string(frame)}
     response = client.detect_labels(Image=request)
     for object in response['Labels']:
         print("{} = {:.2f}%".format(object['Name'], object['Confidence']))
 
 
 class Face:
-    def __init__(self, face_detail):
-        bounding_box = face_detail['BoundingBox']
-        self.top = int(bounding_box['Top'] * HEIGHT)
-        self.left = int(bounding_box['Left'] * WIDTH)
-        width = int(bounding_box['Width'] * WIDTH)
-        height = int(bounding_box['Height'] * HEIGHT)
+    def __init__(self, box, caption="", landmarks=None):
+        self.top = int(box['Top'] * HEIGHT)
+        self.left = int(box['Left'] * WIDTH)
+        width = int(box['Width'] * WIDTH)
+        height = int(box['Height'] * HEIGHT)
         self.right = self.left + width
         self.bottom = self.top + height
-
-        self.landmarks = [
-            (int(landmark['X'] * WIDTH), int(landmark['Y'] * HEIGHT)) for landmark in face_detail['Landmarks']
-        ]
-
+        self.caption = caption
+        if landmarks is None:
+            self.landmarks = None
+        else:
+            self.landmarks = [
+                (int(landmark['X'] * WIDTH), int(landmark['Y'] * HEIGHT)) for landmark in landmarks
+            ]
         self.facebox_alpha = 1.0
 
     def draw_overlay(self, frame):
         overlay = frame.copy()
         cv2.rectangle(overlay, (self.left, self.top), (self.right, self.bottom), (0, 0, 255), 2)
         cv2.rectangle(overlay, (self.left, self.bottom), (self.right, self.bottom), (0, 0, 255), cv2.FILLED)
-        cv2.putText(overlay, "face", (self.left + 6, self.bottom - 6), FONT, 1.0, (255, 255, 255), 1)
-        for (x, y) in self.landmarks:
-            cv2.circle(overlay, (x, y), 4, (255, 0, 0), cv2.FILLED)
-        self.facebox_alpha = self.facebox_alpha - 0.01
+        cv2.putText(overlay, self.caption, (self.left + 6, self.bottom - 6), FONT, 0.5, (255, 255, 255), 1)
+        if self.landmarks is not None:
+            for (x, y) in self.landmarks:
+                cv2.circle(overlay, (x, y), 4, (255, 0, 0), cv2.FILLED)
+        self.facebox_alpha = self.facebox_alpha - 0.1
         cv2.addWeighted(overlay, self.facebox_alpha, frame, 1 - self.facebox_alpha, 0, frame)
-
-
-
 
 
 def detect_faces(frame, faces):
     print('Detecting faces...')
-    request = create_rekognition_request(image_data=video_frame_to_jpeg_string(frame))
-    response = client.detect_faces(Image=request)
+    image = {'Bytes': video_frame_to_jpeg_string(frame)}
+    response = client.detect_faces(Image=image)
     for face_detail in response['FaceDetails']:
-        faces.append(Face(face_detail))
+        faces.append(Face(box=face_detail['BoundingBox'], landmarks=face_detail['Landmarks']))
+
+
+def index_faces(frame, collection_id, face_name):
+    if face_name == "NOONE":
+        print("Can't index unless you specify --face-name")
+        return
+
+    print('Indexing faces...')
+    image = {'Bytes': video_frame_to_jpeg_string(frame)}
+    response = client.index_faces(
+        CollectionId=collection_id,
+        Image=image,
+        ExternalImageId=face_name,
+        DetectionAttributes=['ALL']
+    )
+    print("{}".format(response))
+
+
+def recognise_faces(frame, collection_id, faces):
+    print('Looking for known faces...')
+    image = {'Bytes': video_frame_to_jpeg_string(frame)}
+
+    try:
+        response = client.search_faces_by_image(
+            CollectionId=collection_id,
+            Image=image,
+            MaxFaces=2
+        )
+        print("{}".format(response))
+    except ClientError as e:
+        print("{}".format(e))
+        return
+
+    for face_match in response['FaceMatches']:
+        face = face_match['Face']
+        caption = "{} ({:.2f}%)".format(face['ExternalImageId'], face_match['Similarity'])
+        faces.append(Face(caption=caption, box=response['SearchedFaceBoundingBox']))
 
 
 if __name__ == "__main__":
-    client = create_rekognition_client()
+    args = parse_command_line()
+
+    client = create_rekognition_client(args.region)
     video_capture = cv2.VideoCapture(0)
-    FONT = cv2.FONT_HERSHEY_DUPLEX
+    FONT = cv2.QT_FONT_NORMAL
     WIDTH = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     HEIGHT = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -91,6 +136,12 @@ if __name__ == "__main__":
 
         if key == ord('f'):
             detect_faces(frame, faces)
+
+        if key == ord('i'):
+            index_faces(frame, args.collection_id, args.face_name)
+
+        if key == ord('r'):
+            recognise_faces(frame, args.collection_id, faces)
 
         if key == ord('q'):
             print("Quitting")
